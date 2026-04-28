182adc38b93899e8494f1a9db80fcd9362c7c08b
Timos <psychoson91@gmail.com>
2026-04-28 20:51:17 +0300

    feat : navbar finished

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,6 +5,7 @@ import { Step3PersonTableInput } from './components/step3-person-table-input/ste
 import { Step4ForDirective } from './components/step4-for-directive/step4-for-directive';
 import { Step5EventBind } from './components/step5-event-bind/step5-event-bind';
 import { Navbar } from './components/navbar/navbar';
+import { Step7ListGroupMenu } from './components/step7-list-group-menu/step7-list-group-menu';
 
 import { IPerson } from './shared/interfaces/person';
 
@@ -17,7 +18,8 @@ import { IPerson } from './shared/interfaces/person';
     Step2PersonTable,
     Step3PersonTableInput,
     Step4ForDirective,
-    Step5EventBind],
+    Step5EventBind,
+    Step7ListGroupMenu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/step7-list-group-menu/step7-list-group-menu.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-step7-list-group-menu',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './step7-list-group-menu.html',
+  styleUrl: './step7-list-group-menu.css',
+})
+export class Step7ListGroupMenu {
+  menu = [
+    { text: "Step2 Person Table", appLink:'person-table-example'},
+    { text: "Step3 Component Input", appLink:'component-input-example'},
+    { text: "Step4 @for Directive", appLink:'for-directive-example'},
+    { text: "Step5 Event Bind", appLink:'event-bind-example'},
+    // { text: "Step8 Simple Datatable", appLink:'simple-datatable-example'},
+    // { text: "Step9 Output Example", appLink: 'component-output-example'},
+    // { text: "Step10 Template Forms", appLink:'template-driven-form-example'}
+  ]
+}
